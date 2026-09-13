00356623a60e0025a636689d4e8756cdf4ce0fc9
Anka <runner@sat12-dp148-fe645ae8-9728-4f56-a832-d63ff73e1dbb-92CC676732D6.local>
2026-09-13 09:38:27 +0000

    🍺 update super-engineering to f348a707

--- Casks/super-engineering.rb
+++ Casks/super-engineering.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "super-engineering" do
-  version "6148a7a2"
-  sha256 "efcd6d0b53f54df7531bcb2ec491b0132d5083517747f048b1b70fcf07812f00"
+  version "f348a707"
+  sha256 "50a3c4f2cf31d2157d6aee0a1667cbab045b24e5b858cf6d9a84c5f33ba85373"
 
   url "https://releases.superconductor.so/nightly/Superconductor-nightly-#{version}-arm64.dmg"
   name "super.engineering"
